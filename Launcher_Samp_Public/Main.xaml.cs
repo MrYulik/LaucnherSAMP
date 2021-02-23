abf407f454b6cb927e556c0af5c41dcda308bdb3
MrYulik <73738033+MrYulik@users.noreply.github.com>
2021-02-23 16:28:13 +0200

    Add files via upload

---
+++ Launcher_Samp_Public/Main.xaml.cs
@@ -0,0 +1,128 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Windows.Media;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using static Launcher_Samp_Public.MainWindow;
+using System.Windows.Media.Imaging;
+using System;
+using Microsoft.Win32;
+using System.Diagnostics;
+
+namespace Launcher_Samp_Public
+{
+    public partial class Main : Page
+    {
+        public static readonly string RegistryKey = "HKEY_CURRENT_USER\\SOFTWARE\\SAMP";
+        public Main()
+        {
+            InitializeComponent();
+            News();
+            Playername.Text = Registry.GetValue(RegistryKey, "PlayerName", "").ToString(); ;
+        }
+        private void Play(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (Playername.Text.Length >= 5 && Playername.Text.Length <= 24)
+                {
+                    Registry.SetValue(RegistryKey, "PlayerName", Playername.Text);
+                    Process.Start("samp://" + Api.IP + ":" + Api.PORT);
+                }
+                else
+                    MessageBox.Show("Ваш Nickname не соответствует требованиями! \nNickname должен состоять от 5 и до 24 символов!");
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show("Возможно у Вас не установлен SAMP! Установите его и попробуйте заного. \n\n\n\n Error:\n" + err.Message);
+            }
+         }
+
+        void News()
+        {
+            try
+            {
+                var t = $"eb3e0103eb3e0103eb3e0103d6eb632590eeb3eeb3e0103b26074fca7c9c8eb262fc0a0";
+                IEnumerable<JToken> walls(int id)
+                {
+                    var url = "https://api.vk.com/method/wall.get";
+                    var param = $"?owner_id=-{id}&count=10&extended=0&v=5.68&offset=0&access_token=" + t;
+                    var client = new WebClient();
+                    client.Encoding = System.Text.Encoding.UTF8;
+                    var str = client.DownloadString(url + param);
+                    return JObject.Parse(str)["response"]["items"];
+                }
+                IEnumerable<JToken> NameGroup(int id)
+                {
+                    var url = "https://api.vk.com/method/groups.getById";
+                    var param = $"?group_id={id}&offset=0&access_token=" + t + "&v=5.68";
+                    var client = new WebClient();
+                    client.Encoding = System.Text.Encoding.UTF8;
+                    var str = client.DownloadString(url + param);
+                    return JObject.Parse(str)["response"];
+                }
+                string GName = "";
+                string photo = "";
+                foreach (var items in NameGroup(GroupID))
+                {
+                    GName = items["name"].ToString();
+                    photo = items["photo_50"].ToString();
+                }
+                foreach (var items in walls(GroupID))
+                {
+                    Image ava = new Image
+                    {
+                        Width = 50,
+                        Height = 50,
+                        HorizontalAlignment = HorizontalAlignment.Left,
+                        VerticalAlignment = VerticalAlignment.Center,
+                        SnapsToDevicePixels = true,
+                        UseLayoutRounding = false,
+                        Margin = new Thickness(20, 2, 0, 0),
+                        Stretch = Stretch.UniformToFill,
+                        Source = new BitmapImage(new Uri(photo))
+                    };
+                    StackPanel stack = new StackPanel
+                    {
+                        Background = Brushes.White,
+                        Width = 455,
+                        HorizontalAlignment = HorizontalAlignment.Left,
+                        VerticalAlignment = VerticalAlignment.Center,
+
+                    };
+                    TextBlock textBlock = new TextBlock
+                    {
+                        Margin = new Thickness(80, -20, 21, 0),
+                        TextWrapping = TextWrapping.WrapWithOverflow,
+                        Padding = new Thickness(0, 0, 0, 10),
+                        Text = items["text"].ToString() + "\n\n♥" +
+                                        items["likes"]["count"] + "\t\t\t\t\t☺" + items["views"]["count"]
+                    };
+                    Label name = new Label
+                    {
+                        FontWeight = FontWeights.Bold,
+                        Content = GName,
+                        Margin = new Thickness(80, -45, 0, 0),
+                        Padding = new Thickness(0),
+                        HorizontalAlignment = HorizontalAlignment.Left,
+                        FontSize = 16
+                    };
+                    stack.Children.Add(ava);
+                    stack.Children.Add(name);
+                    stack.Children.Add(textBlock);
+                    ListViewItem item = new ListViewItem
+                    {
+                        Background = Brushes.Aqua,
+                        Width = 430
+                    };
+                    users.Items.Add(stack);
+                    users.Background = Brushes.Aqua;
+                }
+            
+            }
+            catch { }
+        }
+        
+    }
+}
---
+++ Launcher_Samp_Public/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System.Windows;
+using System;
+using System.Net.NetworkInformation;
+using System.Windows.Media;
+using System.Net;
+using System.Diagnostics;
+
+namespace Launcher_Samp_Public
+{
+    /*
+     * Доработан лаунчер
+     * Cover Corporation
+     */
+    public partial class MainWindow : Window
+    {
+        public string IP = "135.181.113.179"; // IP Сервера
+        public int PORT = 9888; // ПОРТ Сервера
+        public static int GroupID = 235422513; // ID Группы
+        public static string SaitURL = "http://covercorp.tk"; // URL Сайта
+        /*
+         * /// ID группы вставлять без vk.com/
+         * Например vk.com/cover_corporation а должно быть cover_corporation
+         */
+
+
+        // НЕ ТРОГАТЬ ЕСЛИ НОВИЧЁК!
+        public string UserName;
+        public static string[] www;
+        public static readonly string RegistryKey = "HKEY_CURRENT_USER\\SOFTWARE\\SAMP";
+        public static readonly string ConfigPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\GTA San Andreas User Files\\SAMP";
+        public static readonly string SAMPConfigPath = ConfigPath + "\\sa-mp.cfg";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Load();
+            Settingcs set = new Settingcs();
+            set.LoadSetting();
+        }
+        
+        
+        void Load()
+        {
+            try
+            {
+                Api.IP = IP;
+                Api.PORT = PORT;
+                Api.Query q = new Api.Query(Api.IP, Api.PORT);
+                q.Send('i');
+                int count = q.Receive();
+                string[] info = q.Store(count);
+                Api.ServerPassword = info[0];
+                Api.Player = info[1];
+                Api.MaxPlayer = info[2];
+                Api.ServerName = info[3];
+                Api.Gamemode = info[4];
+                Api.Language = info[5];
+                Pass.Content = "";
+                if (Api.ServerPassword == "1")
+                {
+                    Pass.Foreground = Brushes.Red;
+                    Pass.Content += "(!)SERVER PASSWORD(!) ";
+                }
+                NameServer.Foreground = Brushes.White;
+                NameServer.Content += Api.ServerName + "\t\tИгроки: " + Api.Player + "/" + Api.MaxPlayer;          
+                Ping();
+            }
+            catch { }
+        }
+        void Ping()
+        {
+            try
+            { 
+                var ping = new Ping();
+                IPAddress addr = IPAddress.Parse(Api.IP);
+                var pingReply = ping.Send(addr);
+                if(pingReply.Status.ToString() != "Success")
+                    MessageBox.Show("Возможно у вас проблемы с интернетом или сервер сейчас не доступен! Попробуйте позже!");
+                pingg.Content = "Ping: "+pingReply.RoundtripTime+"ms";
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show(er.ToString());
+            }
+        }
+        private void Label_Click(object sender, RoutedEventArgs e) => Process.Start("https://vk.com/club" + GroupID);
+        private void LoadMain(object sender, RoutedEventArgs e) => frame.NavigationService.Navigate(new Uri("Main.xaml", UriKind.Relative));
+        private void LoadPageSettings(object sender, RoutedEventArgs e) => frame.NavigationService.Navigate(new Uri("Settings.xaml", UriKind.Relative));
+    }
+}
